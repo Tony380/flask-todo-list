a17716637ce939bae99b8cd44aa22140d01de600
Tony380 <tonygon2@yahoo.es>
2021-03-19 17:28:39 +0100

    end

--- app.py
+++ app.py
@@ -1,10 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.Date, default=datetime.now)
+
+    def __repr__(self):
+        return f'Task {self.id}'
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello World!'
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect('/')
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks = tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        db.session.commit()
+        return redirect('/')
+    
+    else:
+        tasks = Todo.query.get_or_404(id)
+        return render_template('update.html', task = task)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
